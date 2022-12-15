84a5176156e8adbd11d4e54469656f6370b74a38
mohamed allam <55511778+allamo123@users.noreply.github.com>
2022-12-16 00:00:55 +0200

    Change publish to *

--- src/LaravelGrapesServiceProvider.php
+++ src/LaravelGrapesServiceProvider.php
@@ -40,19 +40,12 @@ class LaravelGrapesServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
 
             $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('lg.php'),
                 __DIR__.'/../resources/assets/js/PageBuilder.js' => base_path('public/js/laravel-grapes.js'),
                 __DIR__.'/../resources/assets/css/laravel-grapes.css' => base_path('public/js/laravel-grapes.css'),
-            ], 'assets');
-
-            $this->publishes([
                 __DIR__.'/../database/migrations/2022_11_27_020138_create_pages_table.php' => database_path('/migrations/'.date('Y_m_d_His', time()).'_create_pages_table.php'),
                 __DIR__.'/../database/migrations/2022_12_06_015222_create_custome_blocks_table.php' => database_path('/migrations/'.date('Y_m_d_His', time()).'_create_custome_blocks_table.php'),
-            ], 'migrations');
-
-            $this->publishes([
-              __DIR__.'/../config/config.php' => config_path('lg.php'),
-            ], 'config');
-
+            ], '*');
         }
     }
 
