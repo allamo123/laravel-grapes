7718d3550222f4baa6d68e8f9e535d073f4c3eb0
mohamed allam <55511778+allamo123@users.noreply.github.com>
2022-12-18 09:31:31 +0200

    Fix gitignore

---
+++ src/Http/Controllers/FrontendController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MSA\LaravelGrapes\Http\Controllers;
+
+use Illuminate\Http\Request;
+use MSA\LaravelGrapes\Http\Controllers\Controller;
+
+class FrontendController extends Controller
+{
+
+
+}
